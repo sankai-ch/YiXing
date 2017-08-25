0233e1cfbca01cfa3d17a3a9843a2393c083094e
龙聪 <2987113417@qq.com>
2017-08-25 12:42:08 +0800

    detail修改一点

--- ios_3_Acti/ios_3_Acti/Controller/Activity/DetailViewController.m
+++ ios_3_Acti/ios_3_Acti/Controller/Activity/DetailViewController.m
@@ -7,8 +7,30 @@
 //
 
 #import "DetailViewController.h"
-
+#import <SDWebImage/UIImageView+WebCache.h>
 @interface DetailViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *activityImageView;
+@property (weak, nonatomic) IBOutlet UILabel *applyFeeLbl;
+- (IBAction)applyAction:(UIButton *)sender forEvent:(UIEvent *)event;
+@property (weak, nonatomic) IBOutlet UIButton *applyBtn;
+@property (weak, nonatomic) IBOutlet UILabel *applyStateLbl;
+@property (weak, nonatomic) IBOutlet UILabel *attendenceLbl;
+@property (weak, nonatomic) IBOutlet UILabel *issuerLbl;
+@property (weak, nonatomic) IBOutlet UILabel *typeLbl;
+@property (weak, nonatomic) IBOutlet UILabel *timeLbl;
+@property (weak, nonatomic) IBOutlet UILabel *addressLbl;
+@property (weak, nonatomic) IBOutlet UIButton *phoeBtn;
+- (IBAction)callAction:(UIButton *)sender forEvent:(UIEvent *)event;
+@property (weak, nonatomic) IBOutlet UILabel *applyDueLbl;
+@property (weak, nonatomic) IBOutlet UIView *applyStartView;
+@property (weak, nonatomic) IBOutlet UIView *applStartDueView;
+
+@property (weak, nonatomic) IBOutlet UIView *applendView;
+
+@property (weak, nonatomic) IBOutlet UIView *applyIngeView;
+@property (weak, nonatomic) IBOutlet UILabel *cantentLbl;
+
+@property (strong,nonatomic) NSMutableArray *array;
 
 @end
 
@@ -16,15 +38,20 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+      _array = [NSMutableArray new];
     // Do any additional setup after loading the view.
     [self naviConfig];
+  
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
+- (void) viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    [self networkRequest];
+}
 /*
 #pragma mark - Navigation
 
@@ -52,4 +79,59 @@
     self.navigationController.navigationBar.translucent = YES;
 }
 
+- (IBAction)applyAction:(UIButton *)sender forEvent:(UIEvent *)event {
+}
+- (IBAction)callAction:(UIButton *)sender forEvent:(UIEvent *)event {
+}
+
+
+-(void)networkRequest{
+    //菊花膜
+    UIActivityIndicatorView *aiv = [Utilities getCoverOnView:self.view];
+    
+    NSString *request = [NSString stringWithFormat:@"/event/%@",_activity.activityId];
+      NSMutableDictionary *parameters = [NSMutableDictionary new];
+    if([Utilities loginCheck]){
+        [parameters  setObject:[[StorageMgr singletonStorageMgr] objectForKey:@"MenberId"] forKey:@"memberId"];
+        
+    }
+  
+    
+    [RequestAPI requestURL:request withParameters:parameters andHeader:nil byMethod:kGet andSerializer:kForm success:^(id responseObject) {
+        [aiv stopAnimating];
+        NSLog(@"111%@",responseObject);
+          if ([responseObject[@"resultFlag"] integerValue] == 8001) {
+              
+              NSDictionary *result = responseObject[@"result"];
+              _activity = [[ActivityModel alloc]initWithDetailDictionary:result];
+              [self uiLayout];
+            
+          }
+          else
+          {
+              //业务逻辑失败的情况下
+              NSString *errorMsg = [ErrorHandler getProperErrorString:[responseObject[@"resultFlag"] integerValue]];
+              [Utilities popUpAlertViewWithMsg:errorMsg andTitle:nil onView:self];
+          }
+    }
+    failure:^(NSInteger statusCode, NSError *error)
+    {
+        [aiv stopAnimating];
+        [Utilities popUpAlertViewWithMsg:@"请保持网络连接畅通" andTitle:nil onView:self];
+    }];
+    
+}
+- (void)uiLayout
+{
+    
+    [_activityImageView sd_setImageWithURL:[NSURL URLWithString:_activity.imgUrl] placeholderImage:[UIImage imageNamed:@"png2"]];
+    _applyFeeLbl.text = [NSString stringWithFormat:@"%@元",_activity.applyFee];
+    _attendenceLbl.text = [NSString stringWithFormat:@"%@/%@",_activity.attendence,_activity.limitation];
+    _timeLbl.text = _activity.type;
+    _issuerLbl.text = _activity.issuer;
+    _addressLbl.text = _activity.address;
+    _cantentLbl.text = _activity.content;
+    [_phoeBtn setTitle:[NSString stringWithFormat:@"联系活动发布者:%@",_activity.phone] forState:UIControlStateNormal];
+    
+}
 @end
--- ios_3_Acti/ios_3_Acti/Model/ActivityModel.h
+++ ios_3_Acti/ios_3_Acti/Model/ActivityModel.h
@@ -9,16 +9,26 @@
 #import <Foundation/Foundation.h>
 
 @interface ActivityModel : NSObject
-
+@property (strong,nonatomic) NSString *activityId;
 @property (strong ,nonatomic) NSString *imgUrl;//活动图片URL字符串
 @property (strong ,nonatomic) NSString *name;  //活动名称
 @property (strong ,nonatomic) NSString *content;//活动内容
 @property (nonatomic) NSInteger like;          //活动点赞数
 @property (nonatomic) NSInteger unlike;        //活动被踩数
 @property (nonatomic) BOOL isFavo;             //活动是否被收藏
-
-
+@property (strong ,nonatomic) NSString *address;//活动地址
+@property (strong ,nonatomic) NSString *applyFee;//报名费用
+@property (strong ,nonatomic) NSString *attendence;//报名人数
+@property (strong ,nonatomic) NSString *limitation;//限制人数
+@property (strong ,nonatomic) NSString *type;    //活动类型
+@property (strong ,nonatomic) NSString *issuer; //
+@property (strong ,nonatomic) NSString *phone;
+@property (nonatomic) NSTimeInterval dueTime; //截止时间日期
+@property (nonatomic) NSTimeInterval startTime;
+@property (nonatomic) NSTimeInterval endTime;
+@property (nonatomic) NSInteger status;
 - (id) initWhitDictionary: (NSDictionary *)dict;
+-(id) initWithDetailDictionary:(NSDictionary *)dict;
 
 
 @end
